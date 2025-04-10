98de6411b7e4f91478709d985bb950a8fe44570b
Дарья Базалицкая <mona.kon@yandex.ru>
2025-04-10 23:58:33 +0300

    New try2

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -7,7 +7,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = os.getenv('DEBUG', default=False) == 'True'
+# DEBUG = os.getenv('DEBUG', default=False) == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='*').split(',')
 
